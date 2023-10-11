e2e9b9cbc2c107ba6524e8fa1f9d68f2b06c9187
nahjd <128349111+nahjd@users.noreply.github.com>
2023-10-11 13:27:41 +0400

    Add files via upload

---
+++ 11-UnibankCardsPage/script.js
@@ -0,0 +1,32 @@
+console.log("Hello World"); //Hellow World
+
+let x = 5;
+let y = 2;
+let z = x + y;
+
+console.log(z) // 7
+
+0 == []; //True
+
+0 == "0"; //True
+
+"0" == []; //False
+
+"222" - 22 // returns 200
+
+"ab" - "cd" //returns NaN
+
+>>> a = 257; b = 257
+>>> a is b 
+// True
+let x = 10;
+x -= 5; //5
+
+
+let x = 10;
+x /= 5; //2
+
+let x = -100;
+x >>= 5;  //-4
+
+let x = 16 + "Nahid"; //16Nahid
