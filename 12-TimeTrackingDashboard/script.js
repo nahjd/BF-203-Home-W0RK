aaa96f4502b56a6f17a24b442cc6a1f4f92b8ea4
nahjd <128349111+nahjd@users.noreply.github.com>
2023-10-12 13:22:31 +0400

    Add files via upload

---
+++ 12-TimeTrackingDashboard/script.js
@@ -0,0 +1,85 @@
+// task1
+
+// alert("Enter your namber:");
+// let teref1=Number(prompt("teref 1 daxil edin="));
+// let teref2=Number(prompt("teref 2 daxil edin="));
+// let teref3=Number(prompt("teref 3 daxil edin="));
+
+// if ( teref1 <= 0 || teref2 <= 0 || teref3 <= 0){
+//     alert("Menfi ve sifir teref olanmaz")
+// }
+// else if ( teref1 + teref2 >= teref3 || teref2 + teref3 >= teref1 || teref3 + teref1 >= teref2 ){
+//     alert("Üçbucağın terefleri Ödenir")
+// }
+// else {
+//     alert("Üçbucağın terefleri ödenmir")
+// }
+ 
+
+//  task 2
+// alert("Enter your number:");
+// let teref1=Number(prompt("teref 1 daxil edin="));
+// let teref2=Number(prompt("teref 2 daxil edin="));
+// let teref3=Number(prompt("teref 3 daxil edin="));
+// if ( teref1 <= 0 || teref2 <= 0 || teref3 <= 0){
+//     alert("Menfi ve sifir teref olanmaz")
+// }
+
+// else if( teref1==teref2 && teref2==teref3 && teref1==teref3 ){
+//     alert("Beraberterefli ucbucaqdir")
+// }
+
+// else {
+//     alert("Berabertefeli ucbucaq degil")
+// }
+
+// task3
+// alert("Enter your number:");
+// let teref1=Number(prompt("teref1  daxil edin="));
+// let teref2=Number(prompt("teref2  daxil edin="));
+//    if ( teref1 > teref2) {
+//     alert(`${teref1}  Böyükdür ${teref2}`)
+//    }
+//    else {
+//     alert(`${teref2} Böyükdür ${teref1}`)
+//    }
+
+// task4
+// alert("Enter your number:")
+// let teref1=Number(prompt("teref 1 daxil edin="));
+// let teref2=Number(prompt("teref 2 daxil edin="));
+// let teref3=Number(prompt("teref 3 daxil edin="));
+// if (teref1 >= teref2 && teref1 >= teref3)
+// {
+// alert(`max ${teref1} terefdir`)}
+// else if ( teref2 >= teref1 && teref2 >= teref3){
+//     alert(`max ${teref2} terefdir`)
+// }
+// else if ( teref3 >= teref1 && teref3 >= teref2){
+//     alert(`max ${teref3} terefdir`)
+// }
+
+// task5
+
+// alert("Enter the three number:")
+// let teref1=Number(prompt("teref 1 daxil edin="));
+// let teref2=Number(prompt("teref 2 daxil edin="));
+// let teref3=Number(prompt("teref 3 daxil edin="));
+// if ( teref1 >= teref2 && teref2 >= teref3){
+//     alert(`Ardiciliq ${teref3} ${teref2} ${teref1}`)
+// }
+// else if ( teref2 >= teref1 && teref1 >= teref3 ) {
+//     alert(`Ardiciliq ${teref3} ${teref1} ${teref2} `)
+// }
+// else if ( teref2 >= teref3 && teref3 >= teref1){
+//     alert(`Ardiciliq ${teref1} ${teref3} ${teref2} `)
+// }
+// else if ( teref1 >= teref3 && teref3 >= teref2){
+//     alert(`Ardiciliq ${teref2} ${teref3} ${teref1} `)
+// }
+// else if ( teref3 >= teref1 && teref1 >= teref2){
+//     alert(`Ardiciliq ${teref2} ${teref1} ${teref3} `)
+// }
+// else {
+//     alert(`Diger hallar ${teref1} ${teref2} ${teref3}`)
+// }
